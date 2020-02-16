c4e38c7a4e5de02cf75bf8da669b570f07aee12a
jimmy1982 <jimmybalakrishnan@gmail.com>
2020-02-16 19:17:51 +0530

    our_script

---
+++ our_script.sh
@@ -0,0 +1,15 @@
+#line 7to20 by jimmy
+#get kernel
+get_kernel () {
+echo "Kernel Version is:- `uname -r` "
+}
+
+#get host name
+get_host () {
+echo "Host name is:- `hostname -s` "
+}
+
+#get OS version
+get_osver () {
+echo "OS is:- `uname -s`"
+}
